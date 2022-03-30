bee6e21caf5c137289075caf2bb2bca95a5cfd4d
nrdev <nelson_rojas_janda@hotmail.com>
2022-03-30 10:59:56 +0200

    Add some css

--- js/fetchAsync.dev.js
+++ js/fetchAsync.dev.js
@@ -3,9 +3,11 @@
     const response = await fetch ('https://jsonplaceholder.typicode.com/users');
     const data = await response.json();
     let ctx = '';
+    console.log (data);
     for (let i of data) {
         ctx += `
             <li>${i.name}</li>
+            <li>${i.email}</li>
         `;
     }
     userDiv.innerHTML = ctx;
